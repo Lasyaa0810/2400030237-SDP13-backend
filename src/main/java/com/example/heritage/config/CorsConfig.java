50e9a0487b3ced0deaa81c83319db87eefcfb53c
Lasyaa0810 <2400030237@kluniversity.in>
2026-05-01 20:22:59 +0530

    CORS FIX FINAL

---
+++ src/main/java/com/example/heritage/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.heritage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "https://2400030237-sdp13-frontend-production.up.railway.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
